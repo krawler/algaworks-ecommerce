36acb7e79031546be1d4672d5e79710ebf4cffa6
Rafael Augusto Ramos <august.rafael@gmail.com>
2023-02-04 17:55:37 -0300

    criacao do MapeandoEnumeracoesTest

--- src/test/java/com/algaworks/ecomerce/mapeamentobasico/MapeandoEnumeracoesTest.java
+++ src/test/java/com/algaworks/ecomerce/mapeamentobasico/MapeandoEnumeracoesTest.java
@@ -1,17 +1,29 @@
 package com.algaworks.ecomerce.mapeamentobasico;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 import com.algaworks.ecommerce.jpa.EntityManagerAbstract;
 import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.SexoCliente;
 
 public class MapeandoEnumeracoesTest extends EntityManagerAbstract {
 	
 	@Test
-	private void testEnum() {
+	public void testEnum() {
 		Cliente cliente = new Cliente();
-		cliente.
+		cliente.setNome("Rafael");
+		cliente.setSexoCliente(SexoCliente.MASCULINMO);
 		
+		this.manager.getTransaction().begin();
+		this.manager.persist(cliente);
+		this.manager.getTransaction().commit();
+		
+		this.manager.clear();
+		
+		Cliente clienteVerify = this.manager.find(Cliente.class, 1);
+		assertNotNull(clienteVerify);
 	}
 
 }
